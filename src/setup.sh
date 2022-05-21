087411a658f77d23120606befdc62eb18af7da0e
cosmicoppai <kanakchaudhari12@gmail.com>
2022-05-22 02:11:49 +0530

    add grpc package and protoc installation steps

--- src/setup.sh
+++ src/setup.sh
@@ -1,2 +1,15 @@
+#!/bin/sh
+
+ENV PROTOC_ZIP=protoc-3.13.0-linux-x86_64.zip
+
+apt-get update && apt-get install -y unzip
+
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.13.0/$PROTOC_ZIP \
+    && unzip -o $PROTOC_ZIP -d /usr/local bin/protoc \
+    && unzip -o $PROTOC_ZIP -d /usr/local 'include/*' \
+    && rm -f $PROTOC_ZI
+
+go mod download
 go get -u google.golang.org/grpc
 go get -u github.com/golang/protobuf/protoc-gen-go
+protoc --proto_path proto --go_out=:show  --go-grpc_out=:show --go_opt=paths=import proto/*.proto
